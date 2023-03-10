3e648643dbc6cde21d72e34c5a42f5e84821f364
kiilove <jbkim@jncore.com>
2023-03-10 19:07:36 +0900

    commit on "main"

---
+++ src/pages/PersonalTerms.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PersonalTerms = () => {
+  return (
+    <div className="flex w-full h-full flex-col">
+      <div className="flex w-full">
+        <h1>[개인정보 수집·이용에 대한 동의]</h1>
+      </div>
+    </div>
+  );
+};
+
+export default PersonalTerms;
